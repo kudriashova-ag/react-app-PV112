4112b3c0876e4199d87671483d02a65413133f01
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2023-12-01 19:23:36 +0200

    useEffect, localStorage

--- src/components/ToDo/ToDoFilter.jsx
+++ src/components/ToDo/ToDoFilter.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Button from './Button';
 
-const ToDoFilter = () => {
-    return (
-      <div className="filter">
-        <button>Show All</button>
-        <button>Show Done</button>
-        <button>Show ToDo</button>
-      </div>
-    );
-}
+const ToDoFilter = ({ FILTER_MAP, setFilter, filter }) => {
+  const buttonNames = Object.keys(FILTER_MAP);
+
+  return (
+    <div className="filter">
+      {buttonNames.map((name) => (
+        <Button name={name} setFilter={setFilter} filter={filter} key={name} />
+      ))}
+    </div>
+  );
+};
 
 export default ToDoFilter;
--- src/components/ToDo/ToDoList.jsx
+++ src/components/ToDo/ToDoList.jsx
@@ -1,72 +1,65 @@
-import React, { useState } from "react";
-import './ToDoList.css';
+import React, { useEffect, useState } from "react";
+import "./ToDoList.css";
 import ToDoAddForm from "./ToDoAddForm";
 import ToDoFilter from "./ToDoFilter";
 import ToDoItem from "./ToDoItem";
 
-const items = [
-  {
-    id: 1,
-    title: "Eat",
-    done: true,
-  },
-  {
-    id: 2,
-    title: "Work",
-    done: true,
-  },
-  {
-    id: 3,
-    title: "Sport",
-    done: false,
-  },
-];
-
-
 
 const ToDoList = () => {
+  const items = JSON.parse(localStorage.getItem("items")) || [];
   const [toDoItems, setToDoItems] = useState(items);
+  const [filter, setFilter] = useState("All");
+
+  useEffect(() => { 
+    localStorage.setItem("items", JSON.stringify(toDoItems));
+  }, [toDoItems]);
 
 
-  const addTask = (todoItem) => { 
+
+  const addTask = (todoItem) => {
     const newItems = [...toDoItems, todoItem];
     setToDoItems(newItems);
-  }
+  };
 
   const toggleTaskDone = (id) => {
-    const newItems = toDoItems.map(item => {
+    const newItems = toDoItems.map((item) => {
       if (item.id === id) {
         return { ...item, done: !item.done };
       }
       return item;
     });
     setToDoItems(newItems);
-  }
+  };
 
-  const deleteTask = (id) => { 
-    const newTasks = toDoItems.filter(item => item.id !== id);
+  const deleteTask = (id) => {
+    const newTasks = toDoItems.filter((item) => item.id !== id);
     setToDoItems(newTasks);
-  }
+  };
 
-  const editTask = (id, title) => { 
-    const newTasks = toDoItems.map(item => {
+  const editTask = (id, title) => {
+    const newTasks = toDoItems.map((item) => {
       if (item.id === id) {
-        return { ...item, title: title }
+        return { ...item, title: title };
       }
       return item;
     });
     setToDoItems(newTasks);
-  }
+  };
 
+  const FILTER_MAP = {
+    All: () => true,
+    Done: (task) => task.done,
+    ToDo: (task) => !task.done,
+  };
 
   return (
     <div className="todo-list">
       <h1>ToDo List</h1>
       <ToDoAddForm addTask={addTask} />
       <div>
-        <ToDoFilter />
+        <ToDoFilter FILTER_MAP={FILTER_MAP} setFilter={setFilter} filter={filter} />
         <div className="list">
-          {toDoItems.map((item) => (
+          {toDoItems.filter(FILTER_MAP[filter]).map((item) => (
             <ToDoItem
               task={item}
               key={item.id}
@@ -82,6 +75,3 @@ const ToDoList = () => {
 };
 
 export default ToDoList;
-
-
-
