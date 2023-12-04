b477035de66195a4a9144b130c036ea0d084ee28
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2023-12-04 20:03:41 +0200

    search

--- src/components/Users/UserList.jsx
+++ src/components/Users/UserList.jsx
@@ -1,29 +1,48 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 
 const UserList = () => {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useSearchParams();
+  const [textSearch, setTextSearch] = useState(search.get('s') ?? '');
 
-    const getUsers = async () => { 
-        const response = await axios("https://jsonplaceholder.typicode.com/users");
-        setUsers(response.data);
+  const getUsers = async () => {
+    const response = await axios("https://jsonplaceholder.typicode.com/users");
+    setUsers(response.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const searchHandler = (e) => {
+    setTextSearch(e.target.value);
+    setSearch({ s: e.target.value });
+  };
+
+    const filterUsers = (user) => { 
+        if (textSearch){
+          // return user.name.toLowerCase().includes(textSearch.toLowerCase());
+            return new RegExp(textSearch, "i").test(user.name);
+        }
+        return true;
     }
 
-    useEffect(() => {
-        getUsers();
-     }, []);
+  return (
+    <div>
+      <input type="text" value={textSearch} onChange={searchHandler} />
 
-    return (
-        <div>
-            {users.map((user) => <p>
-                <NavLink to={`/users/${user.id}`}>{user.name}</NavLink>
-            </p>)}
+      {users.filter(filterUsers).map((user) => (
+        <p>
+          <NavLink to={`/users/${user.id}`}>{user.name}</NavLink>
+        </p>
+      ))}
 
-            <Outlet />
-            {/* выводит дочерний компонент */}
-        </div>
-    );
-}
+      <Outlet />
+      {/* выводит дочерний компонент */}
+    </div>
+  );
+};
 
 export default UserList;
