7991d6f24476d82e5d77903a9f80eb40c93c5e85
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2023-12-08 18:16:51 +0200

    factorial

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,17 +2,14 @@ import React, { useRef, useState } from "react";
 
 const Counter = () => {
   let count = useRef(0);
-  
-  const incrementCount = () => { 
-    count.current++;
-    console.log(count.current);
-    console.log(divElem);
-    divElem.current.style.color = 'red'
-  }
-
   const divElem = useRef(null);
 
   let [i, setI] = useState(0);
+
+  const incrementCount = () => { 
+    count.current++;
+    divElem.current.style.color = 'red'
+  }
 
   const increment = () => {
     setI(++i);
@@ -22,15 +19,23 @@ const Counter = () => {
     setI(--i);
   };
 
+  const factorial = factorialOf(i);
+
   return (
     <div ref={divElem}>
       <button onClick={incrementCount}>incrementCount</button>
-
       <button onClick={decrement}>-</button>
       {i}
       <button onClick={increment}>+</button>
+      <div>{factorial}</div>
     </div>
   );
 };
+
+
+function factorialOf(n) {
+  console.log('factorial');
+  return n <= 0 ? 1 : n * factorialOf(n - 1);
+}
 
 export default Counter;
